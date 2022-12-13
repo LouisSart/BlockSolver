d77e28f434098c4e0700833fbda3d06640452515
LouisSart <louissarthou@gmail.fr>
2022-12-13 22:10:02 +0100

    BlockCube test

--- test/block_test.cpp
+++ test/block_test.cpp
@@ -1,7 +1,14 @@
+#include "algorithm.hpp"
 #include "block_cube.hpp"
+#include "cubie_cube.hpp"
 
-int main()
-{
-  const Block<1,3> b("DLB_222",{7},{7,10,11});
-  return 0;
+int main() {
+    const Block<1, 3> b("DLB_222", {7}, {7, 10, 11});
+    BlockCube<1, 3> bc(b);
+    CubieCube cc;
+    Algorithm alg({D2, L, R, F3, U});
+    alg.apply(cc);
+    bc.from_cubie_cube(cc);
+    bc.show();
+    return 0;
 }
