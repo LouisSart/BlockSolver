737ae52df3decf1a5d39bee1b574fb57588db35b
Louis Sarthou <louissarthou@gmail.com>
2023-09-06 18:57:17 +0200

    First breadth first search

--- test/search_test.cpp
+++ test/search_test.cpp
@@ -1,3 +1,4 @@
+#include <deque>
 #include "coordinate_block_cube.hpp"
 #include "move_table.hpp"
 #include "node.hpp"
@@ -9,7 +10,6 @@ void test_expand_cbc() {
     BlockMoveTable table(b);
 
     Node root(cbc, 0);
-    root.show();
     auto children = root.expand(
         [table](const Move& move, CoordinateBlockCube& CBC){table.apply(move,CBC);},
         HTM_Moves
@@ -26,14 +26,38 @@ void test_expand_cbc() {
     assert(children[B3].cube.is_solved());
 }
 
-// void test_breadth_first_search() {
-//     Block<2, 3> b("RouxFirstBlock", {4, 7}, {4, 7, 11});
-//     CoordinateBlockCube cbc;
-//     BlockMoveTable table(b);
+void test_breadth_first_search() {
+    Block<2, 3> b("RouxFirstBlock", {4, 7}, {4, 7, 11});
+    CoordinateBlockCube cbc;
+    BlockMoveTable table(b);
+    table.apply(Algorithm({L, U}), cbc);
 
-// }
+    Node<CoordinateBlockCube> root(cbc, 0);
+    std::deque<Node<CoordinateBlockCube>> queue = {root};
+
+    while (queue.size() != 0) {
+        auto node = queue.back();
+        if (node.cube.is_solved()) { 
+            std::cout << "Solution found :" << std::endl;
+            node.show();
+            return;
+        }
+        else {
+            queue.pop_back();
+            auto children = node.expand(
+                [table](const Move& move, CoordinateBlockCube& CBC){table.apply(move,CBC);},
+                HTM_Moves
+            );
+            for (auto&& child : children) {
+                queue.push_front(child);
+            }
+        }
+        assert(queue.size() < 10000); // Avoiding infinite loop
+    }
+}
 
 int main() {
     test_expand_cbc();
+    test_breadth_first_search();
     return 0;
 }
