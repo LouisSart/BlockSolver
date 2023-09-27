d0e27cb0d78b709cb97974f407b662d0aa2b8cb1
Louis Sarthou <louissarthou@gmail.com>
2023-09-27 13:40:03 +0200

    Search test for 2x2x3 with optimal heuristic

--- test/big_pruning_table_search_test.cpp
+++ test/big_pruning_table_search_test.cpp
@@ -1,18 +1,24 @@
 #include "pruning_table.hpp"
-#include "sys/types.h"
-#include "sys/sysinfo.h"
+#include "search.hpp"
 
-using Table = BlockMoveTable<2, 5>;
 
 int main(){
+    Block<2, 5> b("DL_223", {4, 7}, {4, 7, 8, 10, 11});
+    OptimalPruningTable p_table(b);
+    BlockMoveTable m_table(b);
+    CoordinateBlockCube cbc;
+    Algorithm scramble({R3, U3, F, D2, R2, F3, L2, D2, F3, L, U3, B, U3, D3, F2, B2, L2, D, F2, U2, D, R3, U3, F});
+    m_table.apply(scramble, cbc);
 
-    // Table table; THIS LINE FAILS BECAUSE OF STACK OVERFLOW
-    struct sysinfo memInfo;
-
-    sysinfo (&memInfo);
-    long long freeRAM = memInfo.freeram;
-    //Add other values in next statement to avoid int overflow on right hand side...
-    freeRAM *= memInfo.mem_unit;
-    std::cout << freeRAM << std::endl;
+    Node<CoordinateBlockCube> root(cbc, 0);
+    auto solutions = depth_first_search(
+        root,
+        m_table,
+        p_table,
+        9
+    );
+    for (auto&& s : solutions) {
+        s.show();
+    }
     return 0.;
 }
