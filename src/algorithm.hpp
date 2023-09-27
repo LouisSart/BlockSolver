c73e28dfba00365e6e90cffd245c1668db707065
Louis Sarthou <louissarthou@gmail.com>
2023-09-27 19:15:04 +0200

    Return reference to vector of next moves instead of creating new vector

--- src/algorithm.hpp
+++ src/algorithm.hpp
@@ -52,24 +52,35 @@ void Algorithm::show() {
 std::array<Move, 18> HTM_Moves{U, U2, U3, D, D2, D3, R, R2, R3,
                                L, L2, L3, F, F2, F3, B, B2, B3};
 
-std::vector<Move> allowed_next(const Move m) {
+std::vector<Move> after_U{D,  D2, D3, R,  R2, R3, L, L2,
+                          L3, F,  F2, F3, B,  B2, B3};
+std::vector<Move> after_D{R, R2, R3, L, L2, L3, F, F2, F3, B, B2, B3};
+std::vector<Move> after_R{U,  U2, U3, D,  D2, D3, L, L2,
+                          L3, F,  F2, F3, B,  B2, B3};
+std::vector<Move> after_L{U, U2, U3, D, D2, D3, F, F2, F3, B, B2, B3};
+std::vector<Move> after_F{U,  U2, U3, D,  D2, D3, R, R2,
+                          R3, L,  L2, L3, B,  B2, B3};
+std::vector<Move> after_B{U, U2, U3, D, D2, D3, R, R2, R3, L, L2, L3};
+std::vector<Move> after_None{U, U2, U3, D, D2, D3, R, R2, R3,
+                             L, L2, L3, F, F2, F3, B, B2, B3};
+
+std::vector<Move>& allowed_next(const Move m) {
     // Utility function that takes in the last move applied to the current node
     // and returns the set of all moves of different faces
     switch (m) {
         case U ... U3:
-            return {D, D2, D3, R, R2, R3, L, L2, L3, F, F2, F3, B, B2, B3};
+            return after_U;
         case D ... D3:
-            return {R, R2, R3, L, L2, L3, F, F2, F3, B, B2, B3};
+            return after_D;
         case R ... R3:
-            return {U, U2, U3, D, D2, D3, L, L2, L3, F, F2, F3, B, B2, B3};
+            return after_R;
         case L ... L3:
-            return {U, U2, U3, D, D2, D3, F, F2, F3, B, B2, B3};
+            return after_L;
         case F ... F3:
-            return {U, U2, U3, D, D2, D3, R, R2, R3, L, L2, L3, B, B2, B3};
+            return after_F;
         case B ... B3:
-            return {U, U2, U3, D, D2, D3, R, R2, R3, L, L2, L3};
+            return after_B;
         default:
-            return {U, U2, U3, D, D2, D3, R, R2, R3,
-                    L, L2, L3, F, F2, F3, B, B2, B3};
+            return after_None;
     }
 }
