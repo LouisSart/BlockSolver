26ece6485dfcb0206e3bccfd52ba9ea5e97d0c0a
Louis Sarthou <louissarthou@gmail.com>
2023-10-04 22:23:24 +0200

    csv of the pruning table sizes

--- python/pruning_table_sizes.py
+++ python/pruning_table_sizes.py
@@ -11,14 +11,14 @@ print("--------------------------------------------------------------\n")
 
 print("Exact pruning value : exact number of moves needed to solve the block")
 
-nc_max, ne_max = 5, 6
-sizes = np.empty((nc_max, ne_max), dtype = np.int64)
-for nc in range(nc_max):
-    for ne in range(ne_max):
+nc_max, ne_max = 8, 8
+sizes = np.empty((nc_max+1, ne_max+1), dtype = np.int64)
+for nc in range(nc_max+1):
+    for ne in range(ne_max+1):
         exact_pruning_table_size = 1
-        for c in range(nc+1):
+        for c in range(nc):
             exact_pruning_table_size *= (8-c)*3
-        for e in range(ne+1):
+        for e in range(ne):
             exact_pruning_table_size *= (12-e)*2
         sizes[nc, ne] = exact_pruning_table_size
 
@@ -28,14 +28,14 @@ print(sizes / 1.e6, '\n')
 print("""Permutation pruning value : number of moves needed to restore the block pieces
 to their home positions in the solved permutation state, but they can be misoriented""")
 
-nc_max, ne_max = 5, 6
-sizes = np.empty((nc_max, ne_max), dtype = np.int64)
-for nc in range(nc_max):
-    for ne in range(ne_max):
+nc_max, ne_max = 8, 12
+sizes = np.empty((nc_max+1, ne_max+1), dtype = np.int64)
+for nc in range(nc_max+1):
+    for ne in range(ne_max+1):
         permutation_pruning_table_size = 1
-        for c in range(nc+1):
+        for c in range(nc):
             permutation_pruning_table_size *= (8-c)
-        for e in range(ne+1):
+        for e in range(ne):
             permutation_pruning_table_size *= (12-e)
         sizes[nc, ne] = permutation_pruning_table_size
 
@@ -45,14 +45,14 @@ print(sizes / 1.e6, '\n')
 print("""Orientation pruning value : number of moves needed to restore the block pieces
 to their home positions in the solved orientation, but they can be permuted""")
       
-nc_max, ne_max = 5, 6
-sizes = np.empty((nc_max, ne_max), dtype = np.int64)
-for nc in range(nc_max):
-    for ne in range(ne_max):
+nc_max, ne_max = 8, 12
+sizes = np.empty((nc_max+1, ne_max+1), dtype = np.int64)
+for nc in range(nc_max+1):
+    for ne in range(ne_max+1):
         orientation_pruning_table_size = 1
-        for c in range(nc+1):
+        for c in range(nc):
             orientation_pruning_table_size *= (8-c)*3
-        for e in range(ne+1):
+        for e in range(ne):
             orientation_pruning_table_size *= (12-e)*2
         orientation_pruning_table_size /= math.factorial(nc) * math.factorial(ne)
         sizes[nc, ne] = orientation_pruning_table_size
@@ -64,14 +64,14 @@ print(sizes / 1.e6, '\n')
 print("""Layout pruning value : number of moves needed to restore the block pieces
 to their home positions but they can be permuted and misoriented""")
       
-nc_max, ne_max = 4, 6
-sizes = np.empty((nc_max, ne_max), dtype = np.int64)
-for nc in range(nc_max):
-    for ne in range(ne_max):
+nc_max, ne_max = 8, 12
+sizes = np.empty((nc_max+1, ne_max+1), dtype = np.int64)
+for nc in range(nc_max+1):
+    for ne in range(ne_max+1):
         layout_pruning_table_size = 1
-        for c in range(nc+1):
+        for c in range(nc):
             layout_pruning_table_size *= (8-c)
-        for e in range(ne+1):
+        for e in range(ne):
             layout_pruning_table_size *= (12-e)
         layout_pruning_table_size /= math.factorial(nc) * math.factorial(ne)
         sizes[nc, ne] = layout_pruning_table_size
