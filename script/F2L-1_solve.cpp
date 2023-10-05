0991d85f0dd81143f09ee15ac0f3023975b26d79
Louis Sarthou <louissarthou@gmail.com>
2023-10-05 13:26:47 +0200

    New script directory

---
+++ script/F2L-1_solve.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "block_cube.hpp"
+#include "move_table.hpp"
+#include "pruning_table.hpp"
+
+int main() {
+    auto DLB_222 = Block<1, 3>("DLB_222", {DLB}, {DL, LB, DB});
+    auto DL_223 = Block<2, 5>("DL_223", {DLF, DLB}, {LF, LB, DF, DB, DL});
+    auto DLB_F2Lm1 =
+        Block<3, 7>("DLB_F2L-1", {DLF, DLB, DRB}, {LF, LB, DF, DB, DL, RB, DR});
+    PermutationPruningTable table(DL_223);
+}
