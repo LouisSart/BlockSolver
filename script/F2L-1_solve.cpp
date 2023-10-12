042ad5b539c3d8548ec4253655e28134258cba04
Louis Sarthou <louissarthou@gmail.com>
2023-10-12 23:30:55 +0200

    Script for permutation pruning table for F2L-1 (RAM overflow)

--- script/F2L-1_solve.cpp
+++ script/F2L-1_solve.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
 #include "block_cube.hpp"
-#include "move_table.hpp"
 #include "pruning_table.hpp"
 
+auto LF_column = Block<2, 1>("LF_column", {ULF, DLB}, {LF});
+auto DLB_222 = Block<1, 3>("DLB_222", {DLB}, {DL, LB, DB});
+auto DL_223 = Block<2, 5>("DL_223", {DLF, DLB}, {LF, LB, DF, DB, DL});
+auto DLB_F2Lm1 =
+    Block<3, 7>("DLB_F2L-1", {DLF, DLB, DRB}, {LF, LB, DF, DB, DL, RB, DR});
+
 int main() {
-    auto DLB_222 = Block<1, 3>("DLB_222", {DLB}, {DL, LB, DB});
-    auto DL_223 = Block<2, 5>("DL_223", {DLF, DLB}, {LF, LB, DF, DB, DL});
-    auto DLB_F2Lm1 =
-        Block<3, 7>("DLB_F2L-1", {DLF, DLB, DRB}, {LF, LB, DF, DB, DL, RB, DR});
-    std::cout << "Generating permutation pruning table for Block:" << std::endl;
-    DLB_F2Lm1.show();
-    PermutationPruningTable table(DLB_F2Lm1);
+    auto b = DLB_F2Lm1;
+    PermutationPruningTable table(b);
+    table.gen<true>();
+    // unfortunately this is still too much for my PC, RAM gets overflowed
 }
