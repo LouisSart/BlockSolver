6a1a6fdacb38d1be704e9760dac0eb4bed515be0
Louis Sarthou <louissarthou@gmail.com>
2023-10-16 15:46:59 +0200

    solve src file

--- script/223_solve.cpp
+++ script/223_solve.cpp
@@ -1,31 +1,22 @@
 #include "pruning_table.hpp"
-#include "search.hpp"
+#include "solve.hpp"
 
-template <typename PTable, unsigned nc, unsigned ne>
-auto search(const Block<nc, ne>& b, unsigned expected_number_of_solutions) {
-    PTable p_table(b);
-    BlockMoveTable m_table(b);
-    CoordinateBlockCube cbc;
+int main() {
     Algorithm scramble({R3, U3, F,  D2, R2, F3, L2, D2, F3, L,  U3, B,
                         U3, D3, F2, B2, L2, D,  F2, U2, D,  R3, U3, F});
     scramble.show();
-    m_table.apply(scramble, cbc);
-
-    Node<CoordinateBlockCube> root(cbc, 0);
-    auto solutions = IDAstar(root, m_table, p_table);
-    for (auto&& s : solutions) {
-        s.show();
-    }
-    assert(solutions.size() == expected_number_of_solutions);
-}
-
-int main() {
     auto DL_223 = Block<2, 5>("DL_223", {DLF, DLB}, {LF, LB, DF, DB, DL});
 
     std::cout << "2x2x3 Solve with optimal pruning table" << std::endl;
-    search<OptimalPruningTable<2, 5>>(DL_223, 10);
+    OptimalPruningTable op_table(DL_223);
+    auto solutions = solve(scramble, op_table);
+    show(solutions);
+    assert(solutions.size() == 10);
 
     std::cout << "2x2x3 Solve with permutation pruning table" << std::endl;
-    search<PermutationPruningTable<2, 5>>(DL_223, 10);
+    PermutationPruningTable pe_table(DL_223);
+    solutions = solve(scramble, pe_table);
+    show(solutions);
+    assert(solutions.size() == 10);
     return 0.;
 }
--- script/F2L-1_solve.cpp
+++ script/F2L-1_solve.cpp
@@ -2,7 +2,7 @@
 
 #include "block_cube.hpp"
 #include "pruning_table.hpp"
-#include "search.hpp"
+#include "solve.hpp"
 
 auto LF_column = Block<2, 1>("LF_column", {ULF, DLB}, {LF});
 auto DLB_222 = Block<1, 3>("DLB_222", {DLB}, {DL, LB, DB});
@@ -10,27 +10,16 @@ auto DL_223 = Block<2, 5>("DL_223", {DLF, DLB}, {LF, LB, DF, DB, DL});
 auto DLB_F2Lm1 =
     Block<3, 7>("DLB_F2L-1", {DLF, DLB, DRB}, {LF, LB, DF, DB, DL, RB, DR});
 
-template <typename PTable, unsigned nc, unsigned ne>
-auto search(const Block<nc, ne>& b, unsigned expected_number_of_solutions) {
-    PTable p_table(b);
-    BlockMoveTable m_table(b);
-    CoordinateBlockCube cbc;
+int main() {
     Algorithm scramble({R3, U3, F,  D2, R2, F3, L2, D2, F3, L,  U3, B,
                         U3, D3, F2, B2, L2, D,  F2, U2, D,  R3, U3, F});
     scramble.show();
-    m_table.apply(scramble, cbc);
-
-    Node<CoordinateBlockCube> root(cbc, 0);
-    auto solutions = IDAstar(root, m_table, p_table);
-    for (auto&& s : solutions) {
-        s.show();
-    }
-    assert(solutions.size() == expected_number_of_solutions);
-}
-
-int main() {
     auto b = DLB_F2Lm1;
     PermutationPruningTable table(b);
     // table.gen<true>();
-    search<PermutationPruningTable<3, 7>>(b, 1);
+    auto solutions = solve(scramble, table);
+    for (auto&& s : solutions) {
+        s.show();
+    }
+    assert(solutions.size() == 1);
 }
