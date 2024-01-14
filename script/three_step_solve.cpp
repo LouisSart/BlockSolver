f013090ff79c4ba81b7854288b9e54c556940e6a
Louis Sarthou (Épicier) <louissarthou@gmail.com>
2024-01-13 22:45:30 -0400

    Three step F2L-1 works !

--- script/three_step_solve.cpp
+++ script/three_step_solve.cpp
@@ -19,8 +19,9 @@ auto pruner = Pruner(load_table_ptr(Strategy::Optimal(block_1)),
 
 template <unsigned step>
 auto make_step(const Node<MultiBlockCube<3>>::sptr root, unsigned step_depth) {
-    return IDAstar<false>(root, mover.get_apply(), pruner.get_estimator<step>(),
-                          get_is_solved(root->state, step));
+    return depth_first_search<false>(
+        root, mover.get_apply(), pruner.get_estimator<step>(),
+        get_is_solved(root->state, step), step_depth);
 }
 
 int main() {
@@ -28,30 +29,24 @@ int main() {
     MultiBlockCube<3> cube;
     mover.apply(scramble, cube);
 
-    // Step 1
-    std::cout << "\nStep 1: " << block_1.name << std::endl;
+    // Step 1 : 2x2x2
     auto root = Node<MultiBlockCube<3>>::make_root(cube);
-    auto solutions =
-        IDAstar<true>(root, mover.get_apply(), pruner.get_estimator<0>(),
-                      get_is_solved(cube, 0));
-    show(solutions);
+    auto solutions = make_step<0>(root, 6);
 
-    // Step 2
-    std::cout << "\nStep 2: " << block_2.name << std::endl;
+    // Step 2 : 2x2x3
     auto step2_solutions = Solutions<typename Node<MultiBlockCube<3>>::sptr>();
     for (auto node : solutions) {
-        auto tmp = make_step<1>(node, 7);
+        auto tmp = make_step<1>(node, 11);
         step2_solutions.insert(step2_solutions.begin(), tmp.begin(), tmp.end());
     }
-    show(step2_solutions);
 
-    // Step 3
-    std::cout << "\nStep 3: " << block_3.name << std::endl;
-    MultiBlockCube<3> cube3;
-    mover.apply(step2_solutions[0]->get_path(), cube3);
-    auto root3 = Node<MultiBlockCube<3>>::make_root(cube3);
-    auto step3_solutions = make_step<2>(root3, 10);
-    // IDAstar(root3, mover.get_apply(), pruner.get_estimator<2>(),
-    //         get_is_solved(root3->state, 2));
+    // Step 3 : F2L-1
+    std::cout << "Three step F2L-1 solutions" << std::endl;
+    auto step3_solutions = Solutions<typename Node<MultiBlockCube<3>>::sptr>();
+    for (auto node : step2_solutions) {
+        auto tmp = make_step<2>(node, 16);
+        step3_solutions.insert(step3_solutions.begin(), tmp.begin(), tmp.end());
+    }
+    show(step3_solutions);
     return 0;
 }
