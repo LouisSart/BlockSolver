025b29565528b2c51f4cf1f4e54a80ef42d17d3e
Louis Sarthou (Épicier) <louissarthou@gmail.com>
2024-02-28 17:33:11 -0400

    Consider all symmetries and merge step 2 and 3 in three_step_solve

--- script/three_step_solve.cpp
+++ script/three_step_solve.cpp
@@ -10,15 +10,16 @@ auto method = make_method(Block<1, 3>("DLB_222", {DLB}, {DL, LB, DB}),
                           Block<1, 2>("DB_R_sq", {DRB}, {RB, DR}),
                           Block<0, 4>("TopCross", {}, {UF, UR, UB, UL}));
 
-std::vector<Algorithm> rotations{{},   {x},     {x2},     {x3},
-                                 {y2}, {y2, x}, {y2, x2}, {y2, x3}};
+std::vector<Algorithm> rotations{
+    {},       {x},     {x2},    {x3},     {x, y},  {x2, y}, {x3, y},  {y},
+    {x3, z3}, {z3},    {x, z3}, {x2, z3}, {y2},    {y2, x}, {y2, x2}, {y2, x3},
+    {y, x3},  {x, z3}, {x, y},  {z},      {y3, z}, {x2, y}, {z, y3},  {y3}};
 
 int main(int argc, const char* argv[]) {
     std::array<unsigned, 4> splits_move_counts{
         get_option<unsigned>("-s0", argc, argv),
         get_option<unsigned>("-s1", argc, argv),
-        get_option<unsigned>("-s2", argc, argv),
-        get_option<unsigned>("-s3", argc, argv)};
+        get_option<unsigned>("-s2", argc, argv)};
     auto scramble = Algorithm(argv[argc - 1]);
     scramble.show();
 
@@ -26,14 +27,12 @@ int main(int argc, const char* argv[]) {
 
     // Step 1 : 2x2x2
     solutions = method.make_step<0>(solutions, splits_move_counts[0]);
-    // Step 1 : 2x2x3
-    solutions = method.make_step<0, 1>(solutions, splits_move_counts[1]);
     // Step 1 : F2L-1
-    solutions = method.make_step<0, 1, 2>(solutions, splits_move_counts[2]);
+    solutions = method.make_step<0, 1, 2>(solutions, splits_move_counts[1]);
     // Step 1 : L5C skeleton
-    solutions = method.make_step<0, 1, 2, 3>(solutions, splits_move_counts[3]);
+    solutions = method.make_step<0, 1, 2, 3>(solutions, splits_move_counts[2]);
 
-    std::cout << "Four step corner skeletons" << std::endl;
+    std::cout << "Three step corner skeletons" << std::endl;
     for (auto solution : solutions) {
         show(solution->get_skeleton());
         std::cout << std::endl;
