b74ef6ef1929120b25ba95d4da0dcc5e2c3ff9a8
Louis Sarthou (Épicier) <louissarthou@gmail.com>
2024-03-01 11:03:38 -0400

    2x2x3 Optimal solve now uses Method object

--- script/223_solve.cpp
+++ script/223_solve.cpp
@@ -1,26 +1,27 @@
-#include "pruning_table.hpp"
-#include "solve.hpp"
+#include "option.hpp"
+#include "step.hpp"
 
-using namespace Strategy;
+auto method = make_method(Block<2, 3>("RouxFB", {DLF, DLB}, {DL, LB, LF}),
+                          Block<1, 2>("DL_F_sq", {DLF}, {DF, LF}),
+                          Block<1, 2>("DL_B_sq", {DLB}, {DB, LB}));
 
-int main() {
-    Algorithm scramble({R3, U3, F,  D2, R2, F3, L2, D2, F3, L,  U3, B,
-                        U3, D3, F2, B2, L2, D,  F2, U2, D,  R3, U3, F});
-    auto DL_223 = Block<2, 5>("DL_223", {DLF, DLB}, {LF, LB, DF, DB, DL});
+std::vector<Algorithm> rotations{
+    {},      {x},     {x2}, {x3},   {y},     {y, x},
+    {y, x2}, {y, x3}, {z},  {z, x}, {z, x2}, {z, x3},
+};
 
-    std::cout << "2x2x3 Solve with optimal pruning table" << std::endl;
-    auto solutions = ask_if_generate_and_solve(scramble, Optimal(DL_223));
-    show(solutions);
-    assert(solutions.size() == 10);
+int main(int argc, const char* argv[]) {
+    auto scramble = Algorithm(argv[argc - 1]);
+    scramble.show();
+    auto max_depth = get_option<unsigned>("-d", argc, argv);
 
-    std::cout << "2x2x3 Solve with permutation pruning table" << std::endl;
-    solutions = ask_if_generate_and_solve(scramble, Permutation(DL_223));
-    show(solutions);
-    assert(solutions.size() == 10);
+    auto roots = method.init_roots(scramble, rotations);
+    auto solutions = method.make_step<0, 1, 2>(roots, max_depth);
 
-    std::cout << "2x2x3 Solve with split pruning table" << std::endl;
-    solutions = ask_if_generate_and_solve(scramble, Split(DL_223));
-    show(solutions);
-    assert(solutions.size() == 10);
+    std::cout << "Solutions to 2x2x3" << std::endl;
+    for (auto solution : solutions) {
+        show(solution->get_skeleton());
+        std::cout << std::endl;
+    }
     return 0.;
 }
