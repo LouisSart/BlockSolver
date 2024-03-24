19fbf8a23ef9f03e150a6265aabcdda56f65f774
Louis Sarthou (Épicier) <louissarthou@gmail.com>
2024-03-24 17:07:42 +0100

    Make Node::last_move an Algorithm and add the step_number member

--- src/node.hpp
+++ src/node.hpp
@@ -9,12 +9,14 @@ template <typename Cube>
 struct Node : public std::enable_shared_from_this<Node<Cube>> {
     using sptr = std::shared_ptr<Node<Cube>>;
     using csptr = std::shared_ptr<Node<Cube> const>;
-    Cube state;         // The cube state this node corresponds to
-    sptr parent;        // The shared_ptr to the parent
-    unsigned depth;     // The number of moves made to get this state
-    unsigned estimate;  // The lower-bound estimate on the number of moves
-                        // needed to solve the state
-    Move last_move;     // The move which yielded this state
+
+    Cube state;            // The cube state this node corresponds to
+    sptr parent;           // The shared_ptr to the parent
+    unsigned depth;        // The number of moves made to get this state
+    unsigned estimate;     // The lower-bound estimate on the number of moves
+                           // needed to solve the state
+    Algorithm last_moves;  // The moves which yielded this state
+    int step_number = 0;   // The step which was solved
 
     static sptr make_root(const Cube &cube) { return sptr(new Node(cube, 0)); }
     static sptr make_node(const Cube &cube, const unsigned &depth) {
@@ -24,12 +26,8 @@ struct Node : public std::enable_shared_from_this<Node<Cube>> {
    private:
     Node() : state{Cube()}, depth{0}, parent{nullptr}, estimate{0} {}
     Node(const Cube &c, const unsigned &d = 0, sptr p = nullptr,
-         const Move m = U, const unsigned &e = 1)
-        : state{c}, depth{d}, parent{p}, estimate{e} {
-        if (p != nullptr) {
-            last_move = m;
-        }
-    }
+         const Algorithm &alg = {}, const unsigned &e = 1)
+        : state{c}, depth{d}, parent{p}, last_moves{alg}, estimate{e} {}
 
    public:
     bool is_root() const { return parent == nullptr; }
@@ -45,7 +43,7 @@ struct Node : public std::enable_shared_from_this<Node<Cube>> {
             next = state;
             apply(move, next);
             children.emplace_back(new Node(next, depth + 1,
-                                           this->shared_from_this(), move,
+                                           this->shared_from_this(), {move},
                                            heuristic(next)));
         }
         return children;
@@ -54,27 +52,27 @@ struct Node : public std::enable_shared_from_this<Node<Cube>> {
     Algorithm get_path() const {
         Algorithm path;
         csptr p = this->shared_from_this();
-        while (p->parent != nullptr) {
-            path.append(p->last_move);
+        while (p != nullptr) {
+            path.append(p->last_moves);
             p = p->parent;
         }
         std::reverse(path.sequence.begin(), path.sequence.end());
         return path;
     }
 
-    Algorithm get_sub_path(sptr ancestor) const {
-        Algorithm path;
-        csptr p = this->shared_from_this();
-        while (p != ancestor) {
-            if (ancestor != nullptr) {
-                assert(p->parent != nullptr);  // wrong ancestor ?
-            }
-            path.append(p->last_move);
-            p = p->parent;
-        }
-        std::reverse(path.sequence.begin(), path.sequence.end());
-        return path;
-    }
+    // Algorithm get_sub_path(sptr ancestor) const {
+    //     Algorithm path;
+    //     csptr p = this->shared_from_this();
+    //     while (p != ancestor) {
+    //         if (ancestor != nullptr) {
+    //             assert(p->parent != nullptr);  // wrong ancestor ?
+    //         }
+    //         path.append(p->last_moves);
+    //         p = p->parent;
+    //     }
+    //     std::reverse(path.sequence.begin(), path.sequence.end());
+    //     return path;
+    // }
 
     void show() const {
         std::cout << "Node object: " << std::endl;
