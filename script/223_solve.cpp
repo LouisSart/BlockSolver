da800b789751dc5399573f57b04b16f8628d78c2
Louis Sarthou (Épicier) <louissarthou@gmail.com>
2024-03-24 17:23:35 +0100

    init roots without StepNode

--- script/223_solve.cpp
+++ script/223_solve.cpp
@@ -1,9 +1,13 @@
 #include "option.hpp"
 #include "step.hpp"
 
-auto method = make_method(Block<2, 3>("RouxFB", {DLF, DLB}, {DL, LB, LF}),
-                          Block<1, 2>("DL_F_sq", {DLF}, {DF, LF}),
-                          Block<1, 2>("DL_B_sq", {DLB}, {DB, LB}));
+auto block1 = Block<2, 3>("RouxFB", {DLF, DLB}, {DL, LB, LF});
+auto block2 = Block<1, 2>("DL_F_sq", {DLF}, {DF, LF});
+auto block3 = Block<1, 2>("DL_B_sq", {DLB}, {DB, LB});
+
+auto mover = make_mover(block1, block2, block3);
+
+auto pruner = make_pruner(block1, block2, block3);
 
 std::vector<Algorithm> rotations{
     {},      {x},     {x2}, {x3},   {y},     {y, x},
@@ -14,13 +18,13 @@ int main(int argc, const char* argv[]) {
     auto scramble = Algorithm(argv[argc - 1]);
     scramble.show();
 
-    auto roots = method.init_roots(scramble, rotations);
-    auto solutions = method.make_step<0, 1, 2>(roots, 20, OPT_ONLY);
+    auto roots = init_roots(scramble, rotations, mover);
+    // auto solutions = method.make_step<0, 1, 2>(roots, 20, OPT_ONLY);
 
-    std::cout << "Solutions to 2x2x3" << std::endl;
-    for (auto solution : solutions) {
-        show(solution->get_skeleton());
-        std::cout << std::endl;
-    }
+    // std::cout << "Solutions to 2x2x3" << std::endl;
+    // for (auto solution : solutions) {
+    //     show(solution->get_skeleton());
+    //     std::cout << std::endl;
+    // }
     return 0.;
 }
