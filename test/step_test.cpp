57476a7a631769b2a99d2d3e12fca2f1893194d2
Louis Sarthou (Épicier) <louissarthou@gmail.com>
2024-03-26 02:38:40 +0100

    multiphase solver + test

--- test/step_test.cpp
+++ test/step_test.cpp
@@ -2,6 +2,8 @@
 
 #include "pruning_table.hpp"
 
+using Cube = MultiBlockCube<2>;
+using Sol = Solutions<Node<Cube>::sptr>;
 Block<1, 0> block_1("ULF corner", {ULF}, {});
 Block<0, 1> block_2("UF edge", {}, {UF});
 
@@ -49,20 +51,40 @@ void second_step_test() {
 
 void step_object_test() {
     MultiBlockCube<2> cube;
-    mover.apply({L, F}, cube);
+    mover.apply({L, U3}, cube);
     auto root = make_root(cube);
     root->show();
 
-    auto step = make_block_step<0, 1>(pruner);
-    auto solutions = step.solve_optimal(root, mover, 20, 1);
-    for (auto s : solutions) {
-        s->get_path().show();
-    }
+    auto step = make_block_step<1>(mover, pruner);
+    auto solutions = step.solve_optimal(root);
+    std::cout << "Step 1:" << std::endl;
+    solutions.show();
+
+    auto step2 = make_block_step<0, 1>(mover, pruner);
+    solutions = step2.solve_optimal({solutions[0]});
+    std::cout << "Step 2:" << std::endl;
+    solutions.show();
+}
+
+void multi_phase_test() {
+    std::cout << "Multiphase:" << std::endl;
+    MultiBlockCube<2> cube;
+    mover.apply({L, F, L, R, D}, cube);
+    auto root = make_root(cube);
+
+    auto step = make_block_step<1>(mover, pruner);
+    auto step2 = make_block_step<0, 1>(mover, pruner);
+    std::vector<StepBase<typename Node<Cube>::sptr, decltype(mover)>*> steps = {
+        &step, &step2};
+
+    auto solutions = multi_phase_search(Sol({root}), steps);
+    solutions.show();
 }
 
 int main() {
     step_first_test();
     second_step_test();
     step_object_test();
+    multi_phase_test();
     return 0;
 }
