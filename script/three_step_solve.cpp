3bb743df81520ddba8c9daf35c30b0d1a1ace192
Louis Sarthou (Épicier) <louissarthou@gmail.com>
2024-04-15 15:59:50 -0500

    Merge EO and 2x2x2 step + add command line slackness parameter

--- script/three_step_solve.cpp
+++ script/three_step_solve.cpp
@@ -12,12 +12,10 @@ auto block4 = Block<0, 4>("TopCross", {}, {UF, UR, UB, UL});
 
 auto mover = make_eo_mover(block1, block2, block3, block4);
 auto pruner = make_eo_pruner(block1, block2, block3, block4);
-auto step1 = make_step<0>(mover, pruner);
-auto step2 = make_step<0, 1>(mover, pruner);
-auto step3 = make_step<0, 1, 2>(mover, pruner);
-auto step4 = make_step<0, 1, 2, 3>(mover, pruner);
-auto step5 = make_step<0, 1, 2, 3, 4>(mover, pruner);
-auto steps = make_steps({step1, step2, step3, step4, step5});
+auto steps = make_steps({make_step<0, 1>(mover, pruner),
+                         make_step<0, 1, 2>(mover, pruner),
+                         make_step<0, 1, 2, 3>(mover, pruner),
+                         make_step<0, 1, 2, 3, 4>(mover, pruner)});
 
 std::vector<Algorithm> rotations{
     {},       {x},     {x2},    {x3},     {x, y},  {x2, y}, {x3, y},  {y},
@@ -27,16 +25,18 @@ std::vector<Algorithm> rotations{
 int main(int argc, const char* argv[]) {
     auto scramble = Algorithm(argv[argc - 1]);
     scramble.show();
+    std::vector<unsigned> splits = {
+        get_option("-s0", argc, argv, 10), get_option("-s1", argc, argv, 15),
+        get_option("-s2", argc, argv, 19), get_option("-s3", argc, argv, 23)};
+    unsigned slackness = get_option("-slackness", argc, argv, 1);
 
     auto solutions = init_roots(scramble, rotations, mover);
-
-    // Solve
-    solutions = multi_phase_search(solutions, steps, {5, 10, 15, 20, 21}, 1);
+    solutions = multi_phase_search(solutions, steps, splits, slackness);
 
     solutions.sort_by_depth();
-    std::cout << "Three step corner skeletons" << std::endl;
+    std::cout << "Corner skeletons" << std::endl;
     for (auto sol : solutions) {
-        sol->get_skeleton({"EO", "2x2x2", "2x2x3", "F2L-1", "xC"}).show();
+        sol->get_skeleton({"EO - 2x2x2", "2x2x3", "F2L-1", "xC"}).show();
         std::cout << "-------------" << std::endl;
     }
     return 0;
--- src/option.hpp
+++ src/option.hpp
@@ -9,14 +9,15 @@ bool find_option(const std::string& tag, int argc, const char* argv[]) {
     return false;
 }
 
-template <typename T>
-T get_option(const std::string& tag, int argc, const char* argv[]) {
-    assert(find_option(tag, argc, argv));
-    for (int i = 0; i < argc; ++i) {
-        if (argv[i] == tag) {
-            assert(argv[i + 1][0] != '-');
-            return std::atoi(argv[i + 1]);
+unsigned get_option(const std::string& tag, int argc, const char* argv[],
+                    const unsigned& dflt) {
+    if (find_option(tag, argc, argv)) {
+        for (int i = 0; i < argc; ++i) {
+            if (argv[i] == tag) {
+                assert(argv[i + 1][0] != '-');
+                return std::atoi(argv[i + 1]);
+            }
         }
     }
-    return T();
+    return dflt;
 }
