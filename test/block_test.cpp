52837bbaf0e1eab0d61b93c195134e7306673d4a
Louis Sarthou <louissarthou@gmail.com>
2024-09-03 19:55:25 +0200

    Restore commented test

--- test/block_test.cpp
+++ test/block_test.cpp
@@ -18,34 +18,33 @@ void test_to_cbc_from_cc_and_back(Block<nc, ne> b) {
     assert(cbc == bc.to_coordinate_block_cube(bc.to_cubie_cube(cbc)));
 }
 
-// void test_corner_permutation_table() {
-//     BlockCube<1, 0> bc("UFL corner", {ULF}, {});
-//     CubieCube cc, cc_copy;
-//     uint num_layout = 8;
-//     uint num_perm = 1;
-//     CoordinateBlockCube cbc;
-//     for (uint ccl = 0; ccl < num_layout; ++ccl) {
-//         for (uint ccp = 0; ccp < num_perm; ++ccp) {
-//             cbc.ccl = ccl;
-//             cbc.ccp = ccp;
-//             cc = bc.to_cubie_cube(cbc);
-//             std::cout << "Permutation coordinate " << ccl * num_perm +
-//             ccp
-//                       << ": ";
-//             for (auto move_idx : HTM_Moves) {
-//                 auto move = move_cc[move_idx];
-//                 cc_copy = cc;
-//                 assert(&cc_copy != &cc);
-//                 assert(cc_copy == cc);
-//                 cc_copy.apply(move);
-//                 assert(cc_copy != cc);
-//                 cbc = bc.to_coordinate_block_cube(cc_copy);
-//                 std::cout << cbc.ccl * num_perm + cbc.ccp << " ";
-//             }
-//             std::cout << std::endl;
-//         }
-//     }
-// }
+void test_corner_permutation_table() {
+    BlockCube<1, 0> bc("UFL corner", {ULF}, {});
+    CubieCube cc, cc_copy;
+    unsigned num_layout = 8;
+    unsigned num_perm = 1;
+    CoordinateBlockCube cbc;
+    for (unsigned ccl = 0; ccl < num_layout; ++ccl) {
+        for (unsigned ccp = 0; ccp < num_perm; ++ccp) {
+            cbc.ccl = ccl;
+            cbc.ccp = ccp;
+            cc = bc.to_cubie_cube(cbc);
+            std::cout << "Permutation coordinate " << ccl * num_perm + ccp
+                      << ": ";
+            for (auto move_idx : HTM_Moves) {
+                auto move = move_cc[move_idx];
+                cc_copy = cc;
+                assert(&cc_copy != &cc);
+                assert(cc_copy == cc);
+                cc_copy.apply(move);
+                assert(cc_copy != cc);
+                cbc = bc.to_coordinate_block_cube(cc_copy);
+                std::cout << cbc.ccl * num_perm + cbc.ccp << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+}
 
 int main() {
     test_block();
@@ -55,6 +54,6 @@ int main() {
         "AllCorners", {ULF, URF, URB, ULB, DLF, DRF, DRB, DLB}, {}));
     test_to_cbc_from_cc_and_back(
         Block<0, 4>("BottomCross", {}, {DF, DR, DB, DL}));
-    // test_corner_permutation_table();
+    test_corner_permutation_table();
     return 0;
 }
