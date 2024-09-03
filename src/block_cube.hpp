3bf45cd9fe192f56f3c2230a94334a7514c668b8
Louis Sarthou <louissarthou@gmail.com>
2024-09-03 12:12:42 +0200

    Rewrite BlockCube to simplify

--- src/block_cube.hpp
+++ src/block_cube.hpp
@@ -8,77 +8,34 @@
 #include "coordinate.hpp"
 #include "coordinate_block_cube.hpp"
 #include "cubie_cube.hpp"
+#include "utils.hpp"
 
 template <unsigned nc, unsigned ne>
 struct Block {
     std::array<Corner, nc> corners;  // corner indices
     std::array<Edge, ne> edges;      // edge indices
 
-    // map: [0,...,7]->[block corners,non-block corners]
-    std::array<Corner, NC> c_order;
-    // map: [0,...,11]->[block edges,non-block edges]
-    std::array<Edge, NE> e_order;
-
     std::string name;
     std::string id;
 
     Block() {}
-    Block(std::string bname, const std::array<Corner, nc> &bc,
-          const std::array<Edge, ne> &be)
-        : corners{bc}, edges{be}, name{bname} {
+    // Block(std::string bname, const std::array<Corner, nc> &bc,
+    //       const std::array<Edge, ne> &be)
+    //     : corners{bc}, edges{be}, name{bname} {
+
+    // }
+    Block(std::string n, const std::array<Corner, nc> &c,
+          const std::array<Edge, ne> &e)
+        : name{n}, corners{c}, edges{e} {
+        assert(c.size() == nc);
+        assert(e.size() == ne);
+
         // Sort corners and edges for unicity
         std::sort(corners.begin(), corners.end(),
                   [](const Cubie &c1, const Cubie &c2) { return (c1 < c2); });
         std::sort(edges.begin(), edges.end(),
                   [](const Cubie &e1, const Cubie &e2) { return (e1 < e2); });
-
-        // Compute the run through order of the pieces. Needed for correct
-        // computation of corner and edges layout/permutation coordinates
-        std::list<Corner> c{ULF, URF, URB, ULB, DLF, DRF, DRB, DLB};
-        std::list<Edge> e{UF, UR, UB, UL, LF, RF, RB, LB, DF, DR, DB, DL};
-        for (int i = nc - 1; i >= 0; i--) {  // has to be int because of i >= 0
-            assert((corners[i] >= ULF) && (corners[i] <= DLB));
-            c.remove(corners[i]);
-            c.push_front(corners[i]);
-        }
-        for (int i = ne - 1; i >= 0; i--) {  // has to be int because of i >= 0
-            assert((edges[i] >= UF) && (edges[i] <= DL));
-            e.remove(edges[i]);
-            e.push_front(edges[i]);
-        }
-        Corner *ptr_c_order = c_order.begin();
-        for (auto ptr_c = c.begin(); ptr_c != c.end(); ptr_c++) {
-            *ptr_c_order = *ptr_c;
-            ptr_c_order++;
-        }
-        Edge *ptr_e_order = e_order.begin();
-        for (auto ptr_e = e.begin(); ptr_e != e.end(); ptr_e++) {
-            *ptr_e_order = *ptr_e;
-            ptr_e_order++;
-        }
         id = compute_id();
-    }
-
-    Block(std::string bname, const std::initializer_list<Corner> bc,
-          const std::initializer_list<Edge> be) {
-        assert(bc.size() == nc);
-        assert(be.size() == ne);
-
-        std::array<Corner, nc> car;
-        std::array<Edge, ne> ear;
-        auto lit = bc.begin();
-        for (unsigned k = 0; k < nc; ++k) {
-            assert((ULF <= *lit) && (*lit <= DLB));
-            car[k] = *lit;
-            ++lit;
-        }
-        auto lit_e = be.begin();
-        for (unsigned k = 0; k < ne; ++k) {
-            assert((UF <= *lit_e) && (*lit_e <= DL));
-            ear[k] = *lit_e;
-            ++lit_e;
-        }
-        *this = Block(bname, car, ear);
     };
 
     std::string compute_id() const {
@@ -92,8 +49,8 @@ struct Block {
             edge_positions[e] = 1;
         }
 
-        auto c_id = layout_coord(corner_positions.data(), NC);
-        auto e_id = layout_coord(edge_positions.data(), NE);
+        auto c_id = layout_index(corner_positions, nc);
+        auto e_id = layout_index(edge_positions, ne);
 
         // Block ID is of the form: ncCcid_neEeid
         // For example the DLB 2x2x2 has ID: 1C7_3E217
@@ -101,36 +58,30 @@ struct Block {
                std::to_string(ne) + "E" + std::to_string(e_id);
     }
 
+    bool c_contains(const Cubie &c) const {
+        return (std::find(corners.begin(), corners.end(), c) !=
+                std::end(corners));
+    }
+
+    bool e_contains(const Cubie &e) const {
+        return (std::find(edges.begin(), edges.end(), e) != std::end(edges));
+    }
+
     void show() const {
         std::cout << "Block<" << nc << ", " << ne << ">";
         std::cout << " \"" << name << "\"";
-        std::cout << " c{";
+        std::cout << "\n   Corners: {";
         for (auto &c : corners) {
             std::cout << c << ' ';
         }
-        std::cout << "} e{";
+        if (nc > 0) std::cout << "\b}";
+        std::cout << "\n   Edges: {";
         for (auto &e : edges) {
             std::cout << e << ' ';
         }
-        std::cout << "}" << std::endl;
-        std::cout << "   Corner order: ";
-        for (auto &c : c_order) {
-            std::cout << c << ' ';
-        }
-        std::cout << std::endl;
-        std::cout << "   Edge order: ";
-        for (auto &e : e_order) {
-            std::cout << e << ' ';
-        }
+        if (ne > 0) std::cout << "\b}";
         std::cout << std::endl;
     };
-
-    std::tuple<Block<nc, 0>, Block<0, ne>> split_corners_and_edges() const {
-        Block<nc, 0> corner_part(name + "_corners", corners,
-                                 std::array<Edge, 0>{});
-        Block<0, ne> edge_part(name + "_edges", std::array<Corner, 0>{}, edges);
-        return std::tuple(corner_part, edge_part);
-    }
 };
 
 template <unsigned nc, unsigned ne>
@@ -139,17 +90,17 @@ struct BlockCube {
     // CoordinateBlockCube because we want to avoid reciprocal dependency
     // between CubieCube and CoordinateBlockCube
 
-    unsigned cl[NC];  // Corner layout
-    unsigned el[NE];  // Edge layout
-    unsigned cp[nc];  // Permutation of the block corners
-    unsigned ep[ne];  // Permutation of the block edges
-    unsigned co[nc];  // Orientation of the block corners
-    unsigned eo[ne];  // Orientation of the block edges
-    Block<nc, ne> b;  // Block object
+    std::array<unsigned, NC> cl;  // Corner layout
+    std::array<unsigned, NE> el;  // Edge layout
+    std::array<unsigned, nc> cp;  // Permutation of the block corners
+    std::array<unsigned, ne> ep;  // Permutation of the block edges
+    std::array<unsigned, nc> co;  // Orientation of the block corners
+    std::array<unsigned, ne> eo;  // Orientation of the block edges
+    Block<nc, ne> b;              // Block object
 
     BlockCube(){};
-    BlockCube(std::string bname, const std::initializer_list<Corner> bc,
-              const std::initializer_list<Edge> be)
+    BlockCube(std::string bname, const std::array<Corner, nc> bc,
+              const std::array<Edge, ne> be)
         : b(bname, bc, be){};
     BlockCube(const Block<nc, ne> &b) : b(b){};
 
@@ -158,45 +109,43 @@ struct BlockCube {
         // of the block state in the input CubieCube
 
         CoordinateBlockCube cbc;
-        unsigned kl = 0, k = 0;
-        for (auto &c : b.c_order) {
-            cl[kl] = 0;
-            for (Cubie i = 0; i < nc; i++) {
-                if (cc.cp[c] == b.corners[i]) {
-                    cl[kl] = 1;
-                    cp[k] = i;
-                    co[k] = cc.co[c];
-                    k++;
-                }
+        unsigned k = 0;
+        for (Cubie c = ULF; c <= DLB; ++c) {
+            if (b.c_contains(cc.cp[c])) {
+                assert(k < nc);
+                assert(c < NC);
+                cl[c] = 1;
+                cp[k] = cc.cp[c];
+                co[k] = cc.co[c];
+                ++k;
+            } else {
+                cl[c] = 0;
             }
-            kl++;
         }
 
-        kl = 0, k = 0;
-        for (auto &e : b.e_order) {
-            el[kl] = 0;
-            for (Cubie i = 0; i < ne; i++) {
-                if (cc.ep[e] == b.edges[i]) {
-                    assert(k < 12);
-                    assert(kl < 12);
-                    el[kl] = 1;
-                    ep[k] = i;
-                    eo[k] = cc.eo[e];
-                    k++;
-                }
+        k = 0;
+        for (Cubie e = UF; e <= DL; ++e) {
+            if (b.e_contains(cc.ep[e])) {
+                assert(k < ne);
+                assert(e < NE);
+                el[e] = 1;
+                ep[k] = cc.ep[e];
+                eo[k] = cc.eo[e];
+                k++;
+            } else {
+                el[e] = 0;
             }
-            kl++;
         }
 
         if constexpr (nc > 0) {
-            cbc.ccl = layout_coord(cl, NC);
-            cbc.ccp = perm_coord(cp, nc);
-            cbc.cco = co_coord(co, nc);
+            cbc.ccl = layout_index(cl, nc);
+            cbc.ccp = permutation_index(cp);
+            cbc.cco = co_index(co);
         };
         if constexpr (ne > 0) {
-            cbc.cel = layout_coord(el, NE);
-            cbc.cep = perm_coord(ep, ne);
-            cbc.ceo = eo_coord(eo, ne);
+            cbc.cel = layout_index(el, ne);
+            cbc.cep = permutation_index(ep);
+            cbc.ceo = eo_index(eo);
         };
 
         return cbc;
@@ -204,47 +153,47 @@ struct BlockCube {
 
     CubieCube to_cubie_cube(const CoordinateBlockCube &cbc) {
         // Takes in a CoordinateBlockCube and returns the corresponding
-        // CubieCube All pieces that do not belong to the block are set to
+        // CubieCube. All pieces that do not belong to the block are set to
         // default inconsistent values:
-        // 8 for CP, 12 for EP, 2 for Eo, 3 for CO
+        // 8 for CP, 12 for EP, 2 for EO, 3 for CO
         // This means that the resulting CubieCube cannot be used as a
-        // left multiplicator
+        // right multiplicator
 
         CubieCube cc;
         unsigned k;
         if constexpr (nc > 0) {
-            layout_from_coord(cbc.ccl, NC, nc, cl);
-            perm_from_coord(cbc.ccp, nc, cp);
-            co_from_coord(cbc.cco, nc, co);
+            layout_from_index(cbc.ccl, cl, nc);
+            permutation_from_index(cbc.ccp, cp);
+            co_from_index(cbc.cco, co);
             k = 0;
-            for (Cubie i = 0; i < NC; ++i) {
+            for (Cubie i = ULF; i <= DLB; ++i) {
                 assert(k < NC);
                 if (cl[i] == 1) {
-                    cc.cp[b.c_order[i]] = b.corners[cp[k]];
-                    cc.co[b.c_order[i]] = co[k];
-                    k++;
+                    cc.cp[i] = b.corners[cp[k]];
+                    cc.co[i] = co[k];
+                    ++k;
                 } else {
-                    cc.cp[b.c_order[i]] = NC;
-                    cc.co[b.c_order[i]] = 3;
+                    cc.cp[i] = NC;
+                    cc.co[i] = 3;
                 }
             }
         };
 
         if constexpr (ne > 0) {
-            layout_from_coord(cbc.cel, NE, ne, el);
-            perm_from_coord(cbc.cep, ne, ep);
-            eo_from_coord(cbc.ceo, ne, eo);
+            layout_from_index(cbc.cel, el, ne);
+            permutation_from_index(cbc.cep, ep);
+            eo_from_index(cbc.ceo, eo);
 
             k = 0;
             for (Cubie i = 0; i < NE; i++) {
                 assert(k < NE);
                 if (el[i] == 1) {
-                    cc.ep[b.e_order[i]] = b.edges[ep[k]];
-                    cc.eo[b.e_order[i]] = eo[k];
+                    cc.ep[i] = b.edges[ep[k]];
+                    cc.eo[i] = eo[k];
                     k++;
                 } else {
-                    cc.ep[b.e_order[i]] = NE;
-                    cc.eo[b.e_order[i]] = 2;
+                    cc.ep[i] = NE;
+                    cc.eo[i] = 2;
                 }
             }
         };
--- test/block_test.cpp
+++ test/block_test.cpp
@@ -8,82 +8,44 @@
 void test_block() {
     Block<2, 5> b("DL_223", {DLF, DLB}, {LF, LB, DF, DB, DL});
     b.show();
-    for (auto &c : b.c_order) {
-        assert((ULF <= c) && (c <= DLB));
-    }
-    for (auto &e : b.e_order) {
-        assert((UF <= e) && (e <= DL));
-    }
-    auto [cb, eb] = b.split_corners_and_edges();
-    cb.show();
-    eb.show();
 }
 
 template <unsigned nc, unsigned ne>
 void test_to_cbc_from_cc_and_back(Block<nc, ne> b) {
     BlockCube bc(b);
-    CubieCube cc, return_cc;
-    CoordinateBlockCube cbc;
-    Algorithm alg({D2, L, R, F3, U});
-
-    cc.apply(alg);
-    cbc = bc.to_coordinate_block_cube(cc);
-    return_cc = bc.to_cubie_cube(cbc);
-
-    // Assert cc and return_cc have the block
-    // pieces in the same spots and orientation
-    if (nc > 0) {
-        for (int i = 0; i < 8; ++i) {
-            if (return_cc.cp[i] < 8) {
-                assert(return_cc.cp[i] == cc.cp[i]);
-            }
-
-            if (return_cc.co[i] < 3) {
-                assert(return_cc.co[i] == cc.co[i]);
-            }
-        }
-    }
-
-    if (ne > 0) {
-        for (int i = 0; i < 12; ++i) {
-            if (return_cc.ep[i] < 12) {
-                assert(return_cc.ep[i] == cc.ep[i]);
-            }
-
-            if (return_cc.eo[i] < 2) {
-                assert(return_cc.eo[i] == cc.eo[i]);
-            }
-        }
-    }
+    auto cc = CubieCube::random_state();
+    auto cbc = bc.to_coordinate_block_cube(cc);
+    assert(cbc == bc.to_coordinate_block_cube(bc.to_cubie_cube(cbc)));
 }
 
-void test_corner_permutation_table() {
-    BlockCube<1, 0> bc("UFL corner", {ULF}, {});
-    CubieCube cc, cc_copy;
-    uint num_layout = 8;
-    uint num_perm = 1;
-    CoordinateBlockCube cbc;
-    for (uint ccl = 0; ccl < num_layout; ++ccl) {
-        for (uint ccp = 0; ccp < num_perm; ++ccp) {
-            cbc.ccl = ccl;
-            cbc.ccp = ccp;
-            cc = bc.to_cubie_cube(cbc);
-            std::cout << "Permutation coordinate " << ccl * num_perm + ccp
-                      << ": ";
-            for (auto move_idx : HTM_Moves) {
-                auto move = move_cc[move_idx];
-                cc_copy = cc;
-                assert(&cc_copy != &cc);
-                assert(cc_copy == cc);
-                cc_copy.apply(move);
-                assert(cc_copy != cc);
-                cbc = bc.to_coordinate_block_cube(cc_copy);
-                std::cout << cbc.ccl * num_perm + cbc.ccp << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
-}
+// void test_corner_permutation_table() {
+//     BlockCube<1, 0> bc("UFL corner", {ULF}, {});
+//     CubieCube cc, cc_copy;
+//     uint num_layout = 8;
+//     uint num_perm = 1;
+//     CoordinateBlockCube cbc;
+//     for (uint ccl = 0; ccl < num_layout; ++ccl) {
+//         for (uint ccp = 0; ccp < num_perm; ++ccp) {
+//             cbc.ccl = ccl;
+//             cbc.ccp = ccp;
+//             cc = bc.to_cubie_cube(cbc);
+//             std::cout << "Permutation coordinate " << ccl * num_perm +
+//             ccp
+//                       << ": ";
+//             for (auto move_idx : HTM_Moves) {
+//                 auto move = move_cc[move_idx];
+//                 cc_copy = cc;
+//                 assert(&cc_copy != &cc);
+//                 assert(cc_copy == cc);
+//                 cc_copy.apply(move);
+//                 assert(cc_copy != cc);
+//                 cbc = bc.to_coordinate_block_cube(cc_copy);
+//                 std::cout << cbc.ccl * num_perm + cbc.ccp << " ";
+//             }
+//             std::cout << std::endl;
+//         }
+//     }
+// }
 
 int main() {
     test_block();
@@ -93,6 +55,6 @@ int main() {
         "AllCorners", {ULF, URF, URB, ULB, DLF, DRF, DRB, DLB}, {}));
     test_to_cbc_from_cc_and_back(
         Block<0, 4>("BottomCross", {}, {DF, DR, DB, DL}));
-    test_corner_permutation_table();
+    // test_corner_permutation_table();
     return 0;
 }
