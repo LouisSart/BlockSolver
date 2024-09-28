3d5dfef2f99914ccb15862d6cf9051e3f0768c6e
Louis Sarthou <louissarthou@gmail.com>
2024-09-28 16:07:21 +0200

    factory for single block solver

--- script/222.cpp
+++ script/222.cpp
@@ -3,9 +3,26 @@
 #include "search.hpp"
 #include "step.hpp"
 #include "symmetry.hpp"
+#include "utils.hpp"
+
+template <unsigned nc, unsigned ne, long unsigned NS>
+auto make_optimal_block_solver(Block<nc, ne> &block,
+                               const std::array<unsigned, NS> &rotations) {
+    static auto m_table = BlockMoveTable(block);
+    static auto p_table = load_pruning_table(block);
+    static auto apply = get_sym_apply<NS>(m_table, rotations);
+    static auto estimate = get_estimator<NS>(p_table);
+    static auto is_solved = get_is_solved<NS>(block);
+
+    return [](const auto node) {
+        print(estimate(node->state), is_solved(node->state));
+        // return 1;
+        return IDAstar(node, apply, estimate, is_solved);
+    };
+}
 
 constexpr unsigned NS = 8;  // There are 8 different 2x2x2 symmetries
-using Cube = MultiBlockCube<NS>;
+// using Cube = MultiBlockCube<NS>;
 
 auto block = Block<1, 3>("DLB_222", {DLB}, {LB, DB, DL});
 
@@ -15,11 +32,7 @@ std::array<unsigned, NS> rotations = {
     symmetry_index(0, 0, 1, 0), symmetry_index(0, 1, 1, 0),
     symmetry_index(0, 2, 1, 0), symmetry_index(0, 3, 1, 0)};
 
-auto m_table = BlockMoveTable(block);
-auto p_table = load_pruning_table(block);
-auto apply = get_sym_apply<NS>(m_table, rotations);
-auto estimate = get_estimator<NS>(p_table);
-auto is_solved = get_is_solved<NS>(block);
+auto solve_222 = make_optimal_block_solver(block, rotations);
 
 int main(int argc, const char *argv[]) {
     auto scramble = Algorithm(argv[argc - 1]);
@@ -27,7 +40,7 @@ int main(int argc, const char *argv[]) {
 
     auto root = init_root(scramble, block, rotations);
 
-    auto solutions = IDAstar(root, apply, estimate, is_solved);
+    auto solutions = solve_222(root);
 
     solutions.sort_by_depth();
     solutions.show();
--- src/step.hpp
+++ src/step.hpp
@@ -5,120 +5,6 @@
 #include "pruning_table.hpp"          // load_ptr(Strategy)
 #include "search.hpp"                 // DFS and IDA*
 
-// template <typename... MTs>
-// struct Mover {
-//     static constexpr unsigned _NTABLES = sizeof...(MTs);
-//     using Cube = MultiBlockCube<_NTABLES>;
-//     std::tuple<MTs*...> _mts;
-
-//     Mover() {}
-//     Mover(MTs*... mts) { _mts = std::tuple(mts...); }
-
-//     template <unsigned step>
-//     void apply(const Move& move, MultiBlockCube<_NTABLES>& mbc) const {
-//         static_assert(step < _NTABLES);
-//         std::get<step>(_mts)->apply(move, mbc[step]);
-//         if constexpr (step > 0) {
-//             apply<step - 1>(move, mbc);
-//         }
-//     }
-
-//     void apply(const Move& move, MultiBlockCube<_NTABLES>& mbc) const {
-//         apply<_NTABLES - 1>(move, mbc);
-//     }
-
-//     template <unsigned step>
-//     void apply(const Algorithm& seq, MultiBlockCube<_NTABLES>& mbc) const {
-//         static_assert(step < _NTABLES);
-//         std::get<step>(_mts)->apply(seq, mbc[step]);
-//         if constexpr (step > 0) {
-//             apply<step - 1>(seq, mbc);
-//         }
-//     }
-
-//     template <unsigned step>
-//     void apply_inverse(const Algorithm& seq,
-//                        MultiBlockCube<_NTABLES>& mbc) const {
-//         static_assert(step < _NTABLES);
-//         std::get<step>(_mts)->apply_inverse(seq, mbc[step]);
-//         if constexpr (step > 0) {
-//             apply_inverse<step - 1>(seq, mbc);
-//         }
-//     }
-
-//     void apply(const Algorithm& seq, MultiBlockCube<_NTABLES>& mbc) const {
-//         apply<_NTABLES - 1>(seq, mbc);
-//     }
-
-//     void apply_inverse(const Algorithm& seq,
-//                        MultiBlockCube<_NTABLES>& mbc) const {
-//         apply_inverse<_NTABLES - 1>(seq, mbc);
-//     }
-
-//     auto get_apply() const {
-//         return [this](const Move& move, MultiBlockCube<_NTABLES>& mbc) {
-//             this->apply<_NTABLES - 1>(move, mbc);
-//         };
-//     }
-// };
-
-// template <typename... BlockTypes>
-// auto make_mover(BlockTypes&... blocks) {
-//     return Mover(new BlockMoveTable(blocks)...);
-// }
-
-// template <typename... BlockTypes>
-// auto make_eo_mover(BlockTypes&... blocks) {
-//     return Mover(new EOMoveTable(), new BlockMoveTable(blocks)...);
-// }
-
-// template <typename... PTs>
-// struct Pruner {
-//     static constexpr unsigned _NTABLES = sizeof...(PTs);
-//     using Cube = MultiBlockCube<_NTABLES>;
-
-//     std::tuple<PTs...> _pts;
-
-//     Pruner(PTs... pts) { _pts = std::tuple(pts...); }
-
-//     template <unsigned block>
-//     unsigned get_estimate(const Cube& state) const {
-//         // Return the pruning value for block number `block`
-//         static_assert(block < _NTABLES);
-//         return std::get<block>(_pts).get_estimate(state[block]);
-//     };
-
-//     template <unsigned current, unsigned next, unsigned... more>
-//     unsigned get_estimate(const Cube& state) const {
-//         // Recursively return the max between pruning values for blocks
-//         // number `current`, `next`, and `more...`
-//         auto current_estimate = get_estimate<current>(state);
-//         auto next_estimate = get_estimate<next, more...>(state);
-//         return current_estimate > next_estimate ? current_estimate
-//                                                 : next_estimate;
-//     };
-
-//     template <unsigned... blocks>
-//     auto get_estimator() const {
-//         // Return a lambda that can compute the max
-//         // of all pruning values for all `blocks`
-//         return [this](const Cube& state) -> unsigned {
-//             return this->get_estimate<blocks...>(state);
-//         };
-//     }
-// };
-
-// template <typename... BlockTypes>
-// auto make_pruner(BlockTypes&... blocks) {
-//     return Pruner(load_pruning_table(blocks)...);
-// }
-
-// template <typename... BlockTypes>
-// auto make_eo_pruner(const BlockTypes&... blocks) {
-//     return Pruner(load_eo_table(),
-//                   load_pruning_table(Strategy::Optimal(blocks))...);
-// }
-
 template <std::size_t NB, typename MoveTable>
 auto get_sym_apply(const MoveTable& m_table,
                    const std::array<unsigned, NB>& rotations) {
@@ -153,51 +39,6 @@ auto get_estimator(const PruningTable& p_table) {
     };
 }
 
-// template <typename NodePtr, typename Mover>
-// struct StepBase {
-//     Mover mover;
-//     StepBase() {}
-//     virtual Solutions<NodePtr> solve_optimal(NodePtr root,
-//                                              unsigned max_depth = 20,
-//                                              unsigned slackness = 0) = 0;
-//     virtual ~StepBase() = default;
-// };
-
-// template <typename NodePtr, typename Mover, typename Pruner, unsigned...
-// steps> struct Step : StepBase<NodePtr, Mover> {
-//     using Cube = typename Pruner::Cube;
-//     Pruner pruner;
-//     Mover mover;
-
-//     Step(const Mover& mover, const Pruner& pruner)
-//         : mover{mover}, pruner{pruner} {};
-
-//     Solutions<NodePtr> solve_optimal(NodePtr root, unsigned max_depth = 20,
-//                                      unsigned slackness = 0) {
-//         auto roots = Solutions<NodePtr>({root});
-//         auto node_sols = IDAstar<false>(
-//             roots, mover.get_apply(), pruner.template
-//             get_estimator<steps...>(), get_is_solved<steps...>(root->state),
-//             max_depth, slackness);
-//         node_sols.sort_by_depth();
-//         for (auto node : node_sols) {
-//             node->step_number++;
-//         }
-//         return node_sols;
-//     }
-// };
-
-// template <unsigned... steps, typename Mover, typename Pruner>
-// auto make_step(const Mover& mover, const Pruner& pruner) {
-//     using NodePtr = typename Node<typename Pruner::Cube>::sptr;
-//     return new Step<NodePtr, Mover, Pruner, steps...>(mover, pruner);
-// }
-
-// template <typename Mover, typename Pruner>
-// auto make_steps(std::initializer_list<StepBase<Mover, Pruner>*> args) {
-//     return std::vector<StepBase<Mover, Pruner>*>(args);
-// }
-
 template <typename Block, std::size_t NB>
 auto init_root(const Algorithm& scramble, Block& block,
                const std::array<unsigned, NB> rotations) {
@@ -209,4 +50,18 @@ auto init_root(const Algorithm& scramble, Block& block,
             scramble_cc.get_conjugate(rotations[k]));
     }
     return make_root(ret);
+}
+
+template <unsigned nc, unsigned ne, long unsigned NS>
+auto make_optimal_block_solver(Block<nc, ne>& block,
+                               const std::array<unsigned, NS>& rotations) {
+    static auto m_table = BlockMoveTable(block);
+    static auto p_table = load_pruning_table(block);
+    static auto apply = get_sym_apply<NS>(m_table, rotations);
+    static auto estimate = get_estimator<NS>(p_table);
+    static auto is_solved = get_is_solved<NS>(block);
+
+    return [](const auto node) {
+        return IDAstar(node, apply, estimate, is_solved);
+    };
 }
