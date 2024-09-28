0ac4f0ac2c00f64ed602ae1baed9389415a9c0fe
Louis Sarthou <louissarthou@gmail.com>
2024-09-28 15:59:44 +0200

    add print utility

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,17 +1,37 @@
 #pragma once
 
-void init_array(unsigned* l, unsigned size, unsigned value) {
+void init_array(unsigned *l, unsigned size, unsigned value) {
     for (unsigned i = 0; i < size; ++i) {
         l[i] = value;
     }
 }
 
 template <typename T, std::size_t n, typename cast_t = T>
-void print_array(const typename std::array<T, n>& a) {
+void print_array(const typename std::array<T, n> &a) {
     std::cout << "{";
     for (unsigned k : a) {
         std::cout << static_cast<cast_t>(k) << " ";
     }
     if (n > 0) std::cout << "\b";
     std::cout << "}" << std::endl;
+}
+
+template <typename T, typename cast_t = T>
+void print_vector(const typename std::vector<T> &v) {
+    std::cout << "{";
+    for (auto k : v) {
+        std::cout << static_cast<cast_t>(k) << ", ";
+    }
+    if (v.size() > 0) std::cout << "\b\b";
+    std::cout << "}" << std::endl;
+}
+
+template <typename T, typename... Ts>
+void print(const T &truc, const Ts &...reste) {
+    if constexpr (sizeof...(Ts) == 0) {
+        std::cout << truc << std::endl;
+    } else {
+        std::cout << truc << " ";
+        print(reste...);
+    }
 }
