71d61351fcd0e2552dc4b257504e93ea71329f4a
Louis Sarthou <louissarthou@gmail.com>
2025-07-03 20:13:33 +0200

    fiw multiblockcube show

--- src/coordinate_block_cube.hpp
+++ src/coordinate_block_cube.hpp
@@ -44,11 +44,7 @@ struct MultiBlockCube : std::array<CoordinateBlockCube, nb> {
         std::cout << "MultiBlockCube<" << nb << ">" << std::endl;
         for (unsigned k = 0; k < nb; ++k) {
             std::cout << "   Block " << k << ": ";
-            if ((*this)[k].is_solved()) {
-                std ::cout << "solved" << std::endl;
-            } else {
-                std ::cout << "not solved" << std::endl;
-            }
+            (*this)[k].show();
         }
     }
 };
